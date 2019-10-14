8d92e58f12e710b4e913142c3b93c9f65a6ae655
ValentinZeller <v.zeller@ludus-academie.com>
2019-10-14 16:59:40 +0200

    Structure d'un programme en C

--- Programmer_En_C_NV_V1_0/main.c
+++ Programmer_En_C_NV_V1_0/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define NFOIS 5 //Constante
+
+//Exemple de programme en C
 
 int main()
 {
-    printf("Hello world!\n");
+    int i;
+    float x;
+    float racx;
+
+    printf("Bonjour\n");
+
+    racx=sqrt(x);
+    printf("Calcul de %d racine carré \n",NFOIS);
+    for (i=0; i<NFOIS; i++)
+    {
+        printf("Donnez un nombre : ");
+        scanf("%f",&x); //fonction servent à récupérer la saisie de donnée d'un certain type
+        if (x<0.0) //.0 important car c'est un réel
+            printf("Le nombre %f ne possede pas de racine carree\n",x);
+        else
+        {
+            racx = sqrt(x);
+            printf("Le nombre %f a pour racine carree : %f\n",x,racx);
+        }
+    }
+    printf("Travail termine - Au revoir"); //Pas d'accent console
+
     return 0;
 }
