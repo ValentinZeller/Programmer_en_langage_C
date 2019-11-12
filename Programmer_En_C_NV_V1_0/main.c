d6d44926916e505626b55bda303231424ec0f9ab
ValentinZeller <v.zeller@ludus-academie.com>
2019-11-12 09:32:17 +0100

    Exercice 3 avec static

--- Programmer_En_C_NV_V1_0/main.c
+++ Programmer_En_C_NV_V1_0/main.c
@@ -8,45 +8,42 @@ void f1();
 void f2(int nA);
 int f3(int nA);
 void fct(int p);
-void appel(long *plnAppel,double *pfltPower);
+void appel();
 
 
-int n=5; //déclaration de l'Exerice 2
-
+int n=5; //Exercice 2 : variable globale
 
 int main()
 {
     int i=0;
-    static long lnAppel=0;
-    double fltPower=0;
 
     f1(); //Exercice 1 bonjour 1 fois
     f2(3); //Exercice 1 bonjour autant de fois que le paramètre
     f3(2); //Exercice 1  bonjour autant de fois que le paramètre et return 0
 
-    int n = 3;
-    fct(n); //Affiche 5 (n globale) puis 3(n locale)
+    int n = 3; //Exercice 2 :  variable locale
+    fct(n); //Exercice 2 : Affiche 5 (n globale) puis 3(n locale)
 
-    for (i=0;i<1000000;i++) {
-        appel(&lnAppel,&fltPower);
+    for (i=0;i<1000;i++) {//Exercice 3
+        appel();
     }
 
 
     return 0;
 }
 
-void f1() {
+void f1() {// Exercice 1 : bonjour une fois
     printf("Bonjour\n");
 }
 
-void f2(int nA) {
+void f2(int nA) {// Exercice 1 : bonjour nA fois
     int i = 0;
     for(i=0;i<nA;i++) {
         printf("Bonjour\n");
     }
 }
 
-int f3(int nA) {
+int f3(int nA) { // Exercice 1 : bonjour nA fois et return 0
     int i = 0;
     for(i=0;i<nA;i++) {
         printf("Bonjour\n");
@@ -54,14 +51,16 @@ int f3(int nA) {
     return 0;
 }
 
-void fct(int p) {
+void fct(int p) { //Affiche 5 (n globale) puis 3(n locale)
     printf("%d %d\n",n,p);
 }
 
-void appel(long *plnAppel,double *pfltPower) { //Exercice 3
-    *plnAppel = *plnAppel + 1;
-    if (*plnAppel%(long)pow(10.0,*pfltPower) == 0) {
-        printf("*** appel %d fois ***\n",(int)*plnAppel);
-        *pfltPower = *pfltPower + 1;
+void appel() { //Exercice 3
+    static long lnAppel=0;
+    static double fltPower=0;
+    lnAppel++;
+    if (lnAppel%(long)pow(10.0,fltPower) == 0) {
+        printf("*** appel %d fois ***\n",(int)lnAppel);
+        fltPower++;
     }
 }
