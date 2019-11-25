47b6dc092e3937e9df938b42258d384e90969038
ValentinZeller <v.zeller@ludus-academie.com>
2019-11-25 12:01:21 +0100

    Cours sur les types et enumérations

--- Programmer_En_C_NV_V1_0/main.c
+++ Programmer_En_C_NV_V1_0/main.c
@@ -1,69 +1,85 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-
 #define TAILLE 100
-
 //Les Typedef
-typedef struct tPoint{//Signature du typedef
+
+typedef struct tPoint{//Signature du typdef
+
     int nX;
     int nY;
-}tPoint;//Declaration du nouveau type
 
-typedef struct tDroite{ //Composition : composé d'un tableau de point/tableau de point composant
+}tPoint,*ptPoint;//Declaration du nouveau type
+
+
+
+typedef struct tDroite{
+
     tPoint tabPoint[TAILLE];
+
 }tDroite;
 
 
-void setX(tPoint *pt,int nNewX);
-void setY(tPoint *pt,int nNewY);
-int getX(tPoint pt);
-int getY(tPoint pt);
-void creaDroite(tDroite * dt, tPoint pt,int nIndex);
-tPoint depPoint(tPoint pt,int nNewX,int nNewY);
+//Les enumerations
+typedef  enum couleur{jaune,rouge,bleu,vert} couleur;
+//Commence a index 0
+// Si typedef  enum couleur{jaune=1,rouge=2,bleu,vert} couleur;
+//Alors index commence a 1
 
-int main() {
-	tPoint newPoint = {0,0};
-    setX(&newPoint,5);
-    setY(&newPoint,8);
 
-    tDroite dt;
-    int i;
-    for(i=0;i<TAILLE;i++) {
-        creaDroite(&dt,newPoint,i);
+typedef struct tInfoDroite{
+
+    char cNomDroite[100];
+    tDroite dr;
+    couleur coul;
+
+}tInfoDroite;
+
+
+
+
+int main()
+{
+    //renvoie a la declaration de l'alias de la structure tPoint
+    ptPoint pPoint=NULL;//Cette declaration correspond à un pointeur.
+
+
+    //Creation d'une varialbe de type tPoint
+
+    tPoint pts={0,0}; //Initialisation des membres de la structure
+
+    pPoint=&pts;
+
+    printf("x:%d\n",pPoint->nX);
+    printf("y:%d\n",pPoint->nY);
+
+
+    int i=0;
+    tDroite dr;
+
+    for(i=0;i<TAILLE;i++){
+
+        dr.tabPoint[i]=pts;
+
     }
 
-    newPoint = depPoint(newPoint,2,3);
-    printf("%d\n",newPoint.nX);
-    printf("%d\n",newPoint.nY);
+    tInfoDroite iDroite={"segment",{0,0,1,2},jaune};//Peut generer un warning car nous n'initialisons pas les 100 valeurs.
+    //Initilisation d'un membre de type tInfoDroite
 
-	return 0;
-}
 
-void setX(tPoint *pt,int nNewX) {
-    pt->nX = nNewX;
-}
+    for(i=0;i<TAILLE;i++){
 
-void setY(tPoint *pt,int nNewY) {
-    pt->nY = nNewY;
-}
+        printf("Nom de la droite : %s", iDroite.cNomDroite);
+        printf("x: %d", iDroite.dr.tabPoint[i].nX);
+        printf("y: %d", iDroite.dr.tabPoint[i].nY);
+        printf("y: %d", iDroite.coul);//Cela donne id(int) de la couleur
 
-int getX(tPoint pt) {
-    return pt.nX;
-}
+    }
 
-int getY(tPoint pt) {
-    return pt.nY;
-}
 
-void creaDroite(tDroite * dt, tPoint pt,int nIndex) {
-    dt->tabPoint[nIndex].nX = pt.nX+nIndex;
-    dt->tabPoint[nIndex].nY = pt.nY+nIndex;
-    //dt->tabPoint[nIndex] = pt;
-}
 
-tPoint depPoint(tPoint pt,int nNewX,int nNewY) {
-    pt.nX += nNewX;
-    pt.nY += nNewY;
-    return pt;
+
+
+
+   return 0;
 }
