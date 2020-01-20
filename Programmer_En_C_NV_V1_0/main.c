baf40a2d534d0c4b1012b4c65796be8f92d0901c
ValentinZeller <v.zeller@ludus-academie.com>
2020-01-20 11:18:10 +0100

    Types abstraits

--- Programmer_En_C_NV_V1_0/main.c
+++ Programmer_En_C_NV_V1_0/main.c
@@ -1,59 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define TAILLE 20
+
+//Les Types Abstraits de Données
+
+typedef struct Element Element;
+struct Element
+{
+    int nombre;
+    Element *suivant;
+};
+
+typedef struct Pile Pile;
+struct Pile
+{
+    Element *premier;
+};
+
+Pile *initialiser();
+void empiler(Pile *pile, int nvNombre);
+int depiler(Pile *pile);
+void afficherPile(Pile *pile);
 
 int main ()
 {
-  FILE * pFile;
-  /* Mode texte */
-  char cSaisie[TAILLE];
-  char cAutre[TAILLE];
+    Pile *maPile = initialiser();
+    empiler(maPile, 4);
+    empiler(maPile, 8);
+    empiler(maPile, 15);
+    empiler(maPile, 16);
+    empiler(maPile, 23);
+    empiler(maPile, 42);
+    printf("\nEtat de la pile :\n");
+    afficherPile(maPile);
+    printf("Je depile %d\n", depiler(maPile));
+    printf("Je depile %d\n", depiler(maPile));
+    printf("\nEtat de la pile :\n");
+    afficherPile(maPile);
 
-
-  //Saisie utilisateur
-  printf("Veuillez saisir un texte a inserer\n");
-  fgets(cSaisie,TAILLE,stdin);
-  //Ouverture du fichier
-  pFile = fopen("myfile.txt", "w");
-
-  //Ecriture dans le fichier
-  if (pFile != NULL) {
-    fprintf(pFile,"%s\n",cSaisie);
-    fclose (pFile);
-  }
-
-  pFile = fopen("myfile.txt","r");
-  printf("Le fichier contient :\n");
-
-  //Lecture du fichier
-  if (pFile != NULL) {
-    fgets(cAutre,TAILLE,pFile);
-    printf("%s",cAutre);
-    fclose (pFile);
-  }
-
-
-  /*Mode binaire */
-  int nSaisie;
-  int nAutre;
-
-  printf("Veuillez saisir un entier a inserer\n");
-  scanf("%d",&nSaisie);
-  //fgets(cSaisie,TAILLE,stdin);
-  pFile = fopen("myfile.bin","wb");
-  if (pFile != NULL ) {
-    fwrite(&nSaisie,sizeof(int),sizeof(nSaisie),pFile);
-    fclose(pFile);
-  }
-  printf("Le fichier contient :\n");
-  pFile = fopen("myfile.bin","rb");
-  if (pFile != NULL) {
-    fread(&nAutre,sizeof(int),sizeof(nSaisie),pFile);
-    printf("%d",nAutre);
-    fclose(pFile);
-  }
-
-  return 0;
+    return 0;
 }
 
+Pile *initialiser()
+{
+    Pile *pile = malloc(sizeof(*pile));
+    pile->premier = NULL;
+    return pile;
+}
+
+void empiler(Pile *pile, int nvNombre)
+{
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (pile == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    nouveau->nombre = nvNombre;
+    nouveau->suivant = pile->premier;
+    pile->premier = nouveau;
+}
+
+int depiler(Pile *pile)
+{
+    if (pile == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    int nombreDepile = 0;
+    Element *elementDepile = pile->premier;
+    if (pile != NULL && pile->premier != NULL)
+    {
+        nombreDepile = elementDepile->nombre;
+        pile->premier = elementDepile->suivant;
+        free(elementDepile);
+    }
+    return nombreDepile;
+
+}
+
+void afficherPile(Pile *pile)
+{
+    if (pile == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    Element *actuel = pile->premier;
+
+    while (actuel != NULL)
+    {
+        printf("%d\n", actuel->nombre);
+        actuel = actuel->suivant;
+    }
+    printf("\n");
+}
