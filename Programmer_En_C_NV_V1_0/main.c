830895940177576cda0e051b78aa0aa170adb849
ValentinZeller <v.zeller@ludus-academie.com>
2020-01-27 15:57:20 +0100

    Utilisation de SDL2 (init,changement de couleur,clear,destroy)

--- Programmer_En_C_NV_V1_0/main.c
+++ Programmer_En_C_NV_V1_0/main.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include "SDL2/SDL.h"
+#include <time.h>
 
-#define NFOIS 5 //Constante
-
-//Exemple de programme en C
-
-int main()
+int main(int argc, char *argv[])
 {
-    int i;
-    float x;
-    float racx;
+    srand(time(0));
+    SDL_Window *pWindow=NULL; //Pointeur typedef SDL_Window possède la référence d'une window(Win32)
+    SDL_Renderer *pRenderer=NULL;//Pointeur typedef SDL_Renderer possède la référence du rendu encapsule dans une window
+    SDL_Texture *pTexture=NULL;//Pointeur typedef SDL_Texture possède la référence de l'objet image, materiel ou texture qui sera blite dans le renderer
 
-    printf("Bonjour\n");
-
-    racx=sqrt(x);
-    printf("Calcul de %d racine carré \n",NFOIS);
-    for (i=0; i<NFOIS; i++)
+    if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
     {
-        printf("Donnez un nombre : ");
-        scanf("%f",&x); //fonction servent à récupérer la saisie de donnée d'un certain type
-        if (x<0.0) //.0 important car c'est un réel
-            printf("Le nombre %f ne possede pas de racine carree\n",x);
-        else
-        {
-            racx = sqrt(x);
-            printf("Le nombre %f a pour racine carree : %f\n",x,racx);
+        pWindow = SDL_CreateWindow("SDL_Application",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,780,SDL_WINDOW_SHOWN);
+        if (pWindow != NULL) {
+            pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_PRESENTVSYNC);
+
+            SDL_SetRenderDrawColor(pRenderer,0,0,0,SDL_ALPHA_OPAQUE);
+            SDL_RenderPresent(pRenderer);
+            SDL_Delay(2000);
+
+            SDL_SetRenderDrawColor(pRenderer,255,0,0,SDL_ALPHA_OPAQUE);
+            SDL_RenderClear(pRenderer);
+            SDL_RenderPresent(pRenderer);
+            SDL_Delay(2000);
+
+            int i = 0;
+            while (i < 40)
+            {
+                int red = rand() % 255;
+                int green = rand() % 255;
+                int blue = rand() % 255;
+
+                SDL_SetRenderDrawColor(pRenderer, red, green, blue, 0);
+                SDL_RenderFillRect(pRenderer, NULL);
+                SDL_RenderPresent(pRenderer);
+                SDL_Delay(50);
+
+                i++;
+            }
+
+            SDL_DestroyRenderer(pRenderer);
+            SDL_DestroyWindow(pWindow);
+            SDL_Quit();
         }
+    } else {
+        printf("Erreur d'initialisation de la SDL : %s",SDL_GetError());//on affiche l'erreur
+        return 0;//on sort du programme pour éviter de plus gros problèmes
+
     }
-    printf("Travail termine - Au revoir"); //Pas d'accent console
+
+
 
     return 0;
 }
